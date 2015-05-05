6dc7ef91fe7abdd85df885d5bd7bfbac4e0dd47d
Dan Farrow <dan@squarebracket.net>
2015-05-05 13:41:48 +0100

    Some slow progress...

--- source/lib/main.js
+++ source/lib/main.js
@@ -1,4 +1,4 @@
-// Add a toolbar button
+// Add a toolbar button to open the bookmark search page
 let buttons = require('sdk/ui/button/action');
 var button = buttons.ActionButton({
   id: "metabookmarks",
@@ -9,110 +9,15 @@ var button = buttons.ActionButton({
     "64": "./book_open_bookmark-64.png"
   },
   onClick: handleMetaBookmarksClick
-  //onClick: handlerAttempt
 });
-
-//setUpPageMod( );
 
 /**
  * Metabookmarks toolbar button handler
+ *
+ * @todo Load search-bookmarks-by-tag.html
  */
 function handleMetaBookmarksClick( state ){
 	showBookmarksByTag( 'project_javascript' );
-}
-
-/*
-function handlerAttempt(){
-	// Attempting custom protocol for bookmark://
-	let {Cc, Ci} = require( "chrome" );
-	let xpcom = require( "sdk/platform/xpcom" );
-
-	var handler = Cc[ "@mozilla.org/uriloader/web-handler-app;1" ]
-	    .createInstance( Ci.nsIWebHandlerApp );
-
-	// Handle bookmark: protocol
-	handler.name = 'Bookmark protocol';
-	handler.uriTemplate = 'http://localhost/bookmark/#%s';
-
-	var eps = Cc[ "@mozilla.org/uriloader/external-protocol-service;1" ].
-	getService( Ci.nsIExternalProtocolService );
-
-	var handlerInfo = eps.getProtocolHandlerInfo( 'bookmark' );
-	handlerInfo.possibleApplicationHandlers.appendElement(handler, false);
-	handlerInfo.alwaysAskBeforeHandling = false;// don't ask the user
-	handlerInfo.preferredApplicationHandler = handler;  // set my handler as default
-	hi = handlerInfo;
-
-	var hs = Cc[ "@mozilla.org/uriloader/handler-service;1" ].getService( Ci.nsIHandlerService );
-	hs.store(handlerInfo);
-}
-
-/**
- * HACK Use `localhost/bookmarks/*` as a handle
- * to request a page showing a list of bookmarks
- *
- * @todo How to then trigger the doo-dah?
- */
-function setUpPageMod(){
-	pattern = "chrome://metabookmarks/content/test.html";
-	let pageMod = require( "sdk/page-mod" );
-	pageMod.PageMod({
-		include: pattern,
-		contentScript: 'document.body.innerHTML = "<h1>Page matches ruleset</h1>";'
-	});
-}
-
-
-/**
- * Make tests bookmarks
- *
- * @return Null
- */
-function makeTestBookmarks () {
-	makeTestBookmark( "Test bookmark 1", "http://example.com#1", ["test"] );
-	makeTestBookmark( "Test bookmark 2", "http://example.com#2", ["test", "banana"] );
-	makeTestBookmark( "Test bookmark 3", "http://example.com#3", ["test", "orange"] );
-	output ( 'Test bookmarks made. <a target="_blank" href="http://localhost/bookmarks/test">Click here</a>' );
-}
-
-/**
- * Make some test bookmarks for testing testness
- *
- * @return Null
- */
-function makeTestBookmark( title, url, tags ) {
-	// Convert tags to array
-	if ( "string" === typeof tags ){
-		tags = [ tags ];
-	}
-
-	let { Bookmark, save } = require( "sdk/places/bookmarks" );
-
-	// Create a new bookmark instance, unsaved
-	let bookmark = Bookmark({ title: title, url: url, tags: tags });
-
-	// Attempt to save the bookmark instance to the Bookmarks database
-	// and store the emitter
-	let emitter = save( bookmark );
-
-	/*
-
-	// Listen for events
-	emitter.on( "data", function ( saved, inputItem ) {
-	  // on a "data" event, an item has been updated, passing in the
-	  // latest snapshot from the server as `saved` (with properties
-	  // such as `updated` and `id`), as well as the initial input
-	  // item as `inputItem`
-	  console.log( saved.title === inputItem.title ); // true
-	  console.log( saved !== inputItem ); // true
-	  console.log( inputItem === bookmark ); // true
-	}).on( "end" , function ( savedItems, inputItems ) {
-	  // Similar to "data" events, except "end" is an aggregate of
-	  // all progress events, with ordered arrays as `savedItems`
-	  // and `inputItems`
-	});
-
-    */
 }
 
 /**
@@ -200,3 +105,77 @@ function clearOutput( tab ){
 		contentScript: 'document.body.innerHTML = ""'
 	});
 }
+
+
+/* DUMP! */
+
+/*
+function handlerAttempt(){
+	// Attempting custom protocol for bookmark://
+	let {Cc, Ci} = require( "chrome" );
+	let xpcom = require( "sdk/platform/xpcom" );
+
+	var handler = Cc[ "@mozilla.org/uriloader/web-handler-app;1" ]
+	    .createInstance( Ci.nsIWebHandlerApp );
+
+	// Handle bookmark: protocol
+	handler.name = 'Bookmark protocol';
+	handler.uriTemplate = 'http://localhost/bookmark/#%s';
+
+	var eps = Cc[ "@mozilla.org/uriloader/external-protocol-service;1" ].
+	getService( Ci.nsIExternalProtocolService );
+
+	var handlerInfo = eps.getProtocolHandlerInfo( 'bookmark' );
+	handlerInfo.possibleApplicationHandlers.appendElement(handler, false);
+	handlerInfo.alwaysAskBeforeHandling = false;// don't ask the user
+	handlerInfo.preferredApplicationHandler = handler;  // set my handler as default
+	hi = handlerInfo;
+
+	var hs = Cc[ "@mozilla.org/uriloader/handler-service;1" ].getService( Ci.nsIHandlerService );
+	hs.store(handlerInfo);
+}
+
+/*
+function makeTestBookmarks () {
+	makeTestBookmark( "Test bookmark 1", "http://example.com#1", ["test"] );
+	makeTestBookmark( "Test bookmark 2", "http://example.com#2", ["test", "banana"] );
+	makeTestBookmark( "Test bookmark 3", "http://example.com#3", ["test", "orange"] );
+	output ( 'Test bookmarks made. <a target="_blank" href="http://localhost/bookmarks/test">Click here</a>' );
+}
+*/
+
+
+/*
+function makeTestBookmark( title, url, tags ) {
+	// Convert tags to array
+	if ( "string" === typeof tags ){
+		tags = [ tags ];
+	}
+
+	let { Bookmark, save } = require( "sdk/places/bookmarks" );
+
+	// Create a new bookmark instance, unsaved
+	let bookmark = Bookmark({ title: title, url: url, tags: tags });
+
+	// Attempt to save the bookmark instance to the Bookmarks database
+	// and store the emitter
+	let emitter = save( bookmark );
+
+	// Listen for events
+	emitter.on( "data", function ( saved, inputItem ) {
+	  // on a "data" event, an item has been updated, passing in the
+	  // latest snapshot from the server as `saved` (with properties
+	  // such as `updated` and `id`), as well as the initial input
+	  // item as `inputItem`
+	  console.log( saved.title === inputItem.title ); // true
+	  console.log( saved !== inputItem ); // true
+	  console.log( inputItem === bookmark ); // true
+	}).on( "end" , function ( savedItems, inputItems ) {
+	  // Similar to "data" events, except "end" is an aggregate of
+	  // all progress events, with ordered arrays as `savedItems`
+	  // and `inputItems`
+	});
+
+
+}
+*/
